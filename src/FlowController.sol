9277ede2ee66967a4644530d8d5a0125422bcb67
0xGabi <gabrielpk.18@gmail.com>
2022-04-21 15:18:45 -0300

    feat(contract): add supertoken reference

--- src/FlowController.sol
+++ src/FlowController.sol
@@ -1,20 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ConvictionVoting} from "@1hive/apps-conviction-voting/contracts/ConvictionVoting.sol";
+import {Superfluid} from "@blossom-labs/apps-superfluid/contracts/Superfluid.sol";
+import {ISuperToken} from "@blossom-labs/apps-superfluid/contracts/interfaces/ISuperToken.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {Superfluid} from "@blossom-labs/apps-superfluid/contracts/Superfluid.sol";
-import {ConvictionVoting} from "@1hive/apps-conviction-voting/contracts/ConvictionVoting.sol";
 import {ABDKMath64x64} from "abdk-libraries-solidity/ABDKMath64x64.sol";
+
 import "hardhat/console.sol";
 
 contract FlowController is Ownable {
     using ABDKMath64x64 for int128;
     using ABDKMath64x64 for uint256;
     using SafeMath for uint256;
-    using SafeERC20 for ERC20;
 
     // Shift to left to leave space for decimals
     int128 private constant ONE = 1 << 64;
@@ -26,6 +25,7 @@ contract FlowController is Ownable {
 
     ConvictionVoting public cv;
     Superfluid public superfluid;
+    ISuperToken public token;
 
     int128 internal decay;
     int128 internal maxRatio;
@@ -34,9 +34,14 @@ contract FlowController is Ownable {
     mapping(uint256 => Proposal) internal proposals;
     mapping(uint256 => bool) internal activeProposals;
 
-    constructor(ConvictionVoting _cv, Superfluid _superfluid) {
+    constructor(
+        ConvictionVoting _cv,
+        Superfluid _superfluid,
+        ISuperToken _token
+    ) {
         cv = _cv;
         superfluid = _superfluid;
+        token = _token;
 
         decay = cv.decay.divu(1e18).add(1);
         maxRatio = cv.maxRatio.divu(1e18).add(1);
@@ -44,13 +49,17 @@ contract FlowController is Ownable {
     }
 
     function activateProposal(uint256 _proposalId) public onlyOwner {
+        (, , beneficiary) = cv.getProposal(_proposalId);
+
         activeProposals[_proposalId] = true;
-        superfluid.createFlow(_token, _receiver, _flowRate, _description);
+        superfluid.createFlow(token, beneficiary, 0, bytes(0));
     }
 
     function deactivateProposal(uint256 _proposalId) public onlyOwner {
+        (, , beneficiary) = cv.getProposal(_proposalId);
+
         activeProposals[_proposalId] = false;
-        superfluid.deleteFlow(_token, _receiver);
+        superfluid.deleteFlow(token, beneficiary);
     }
 
     function updateActiveProposals(uint256[] _proposalsIds) external {
@@ -64,8 +73,10 @@ contract FlowController is Ownable {
             proposal.lastRate = rate(_proposalId);
             proposal.lastTime = block.timestamp;
 
+            (, , beneficiary) = cv.getProposal(_proposalsIds[i]);
+
             // update flow
-            superfluid.updateFlow(_token, _receiver, _flowRate, _description);
+            superfluid.updateFlow(token, beneficiary, proposal.lastRate, bytes(0));
         }
     }
 
@@ -80,7 +91,7 @@ contract FlowController is Ownable {
         if (_stake == 0) {
             _targetRate = 0;
         } else {
-            uint256 funds = ERC20(cv.requestToken).balanceOf(address(cv.fundsManager));
+            uint256 funds = ISuperToken(token).balanceOf(address(cv.fundsManager));
             uint256 _minStake = minStake();
             _targetRate = (ONE.sub(_minStake.divu(_stake > _minStake ? _stake : _minStake).sqrt())).mulu(
                 maxRatio.mulu(funds)
