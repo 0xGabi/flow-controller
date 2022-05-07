592c404f2d9d64dad831a2c48fb281215b0955d5
Gabi <gabrielpk.18@gmail.com>
2022-05-07 00:44:48 -0300

    fix: add flow setting values

--- deploy/001_deploy_flow_controller.ts
+++ deploy/001_deploy_flow_controller.ts
@@ -3,6 +3,10 @@ import {DeployFunction} from 'hardhat-deploy/types';
 
 import {getConfig} from '../helpers';
 
+const DECAY = String(0.9999999e18);
+const MAX_RATIO = String(Math.floor(0.02e18 / (30 * 24 * 60 * 60)));
+const WEIGHT = String(0.025e18);
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts, run} = hre;
   const {deploy} = deployments;
@@ -14,7 +18,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   await deploy('FlowController', {
     from: deployer,
-    args: [conviction, superfluid, superToken],
+    args: [conviction, superfluid, superToken, DECAY, MAX_RATIO, WEIGHT],
     log: true,
     autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
   });
